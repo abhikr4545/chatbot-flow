4fc5b37a2d33463736fe641465be4e7abb1befa4
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Pro.local>
2024-06-22 21:40:53 +0530

    node types fix

--- src/data/nodeTypes.tsx
+++ src/data/nodeTypes.tsx
@@ -3,7 +3,4 @@ import { TbMessage } from "react-icons/tb";
 
 export const nodetypes = [
   { id: nanoid(), name: "Message", type: "textMessage", icon: <TbMessage size={24} color="#60A5FA" /> },
-  { id: nanoid(), name: "Message", type: "textMessage", icon: <TbMessage size={24} color="#60A5FA" /> },
-  { id: nanoid(), name: "Message", type: "textMessage", icon: <TbMessage size={24} color="#60A5FA" /> },
-  { id: nanoid(), name: "Message", type: "textMessage", icon: <TbMessage size={24} color="#60A5FA" /> },
 ]
